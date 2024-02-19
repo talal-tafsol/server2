473c8470a9acedbf573b31d8012b57a67b1ff1b0
talal-tafsol <97878196+talal-tafsol@users.noreply.github.com>
2024-02-19 18:44:30 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Hello from Server 2",
+  });
+});
+
+app.get("/test", (req, res) => {
+  res.status(200).json({
+    message: "Hello from Test Server 2",
+  });
+});
+
+app.listen(PORT, () => console.log(`Server 2 is running on port: ${PORT}`));
